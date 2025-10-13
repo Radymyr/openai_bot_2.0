d6ed1ae5e3f1d4a1f44e7b60dbf80e28b881ec73
Radymyr <lobodaradymyr@gmail.com>
2025-10-13 17:12:57 +0300

    changed emji

--- src/handleReaction.js
+++ src/handleReaction.js
@@ -6,7 +6,7 @@ export const handleReaction = async (ctx) => {
   console.log(chatId, messageId, "function was run");
   try {
     await bot.telegram.setMessageReaction(chatId, messageId, [
-      { type: "emoji", emoji: "👍" },
+      { type: "emoji", emoji: "☃" },
     ]);
   } catch (err) {
     console.error(err);
