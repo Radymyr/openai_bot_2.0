6af25f8c05a1df7fe1b42d7133271c9555096e74
Radymyr <lobodaradymyr@gmail.com>
2025-10-13 17:37:04 +0300

    added random reaction

--- src/handleReaction.js
+++ src/handleReaction.js
@@ -1,13 +1,18 @@
 import { bot } from "./initializers.js";
+import { reactions } from "./constans/reactions.js";
 
 export const handleReaction = async (ctx) => {
-  const chatId = ctx.message?.chat.id;
-  const messageId = ctx.message?.message_id;
-  try {
-    await bot.telegram.setMessageReaction(chatId, messageId, [
-      { type: "emoji", emoji: "❤‍🔥" },
-    ]);
-  } catch (err) {
-    console.error(err);
+  if (Math.random() <= 0.3) {
+    const chatId = ctx.message?.chat.id;
+    const messageId = ctx.message?.message_id;
+
+    const randomReaction = Math.floor(Math.random() * reactions.length);
+    try {
+      await bot.telegram.setMessageReaction(chatId, messageId, [
+        { type: "emoji", emoji: randomReaction },
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
