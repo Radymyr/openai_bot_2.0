38cd474f74d8d838e545adc3d201cadffbe04c43
Radymyr <lobodaradymyr@gmail.com>
2025-10-13 15:50:59 +0300

    added test reaction

--- src/handleReaction.js
+++ src/handleReaction.js
@@ -3,7 +3,7 @@ import { bot } from "./initializers.js";
 export const handleReaction = async (ctx) => {
   const chatId = ctx.message?.chat.id;
   const messageId = ctx.message?.message_id;
-
+  console.log(chatId, messageId, "function was run");
   try {
     await bot.telegram.setMessageReaction(chatId, messageId, [
       { type: "emoji", emoji: "🎅" },
