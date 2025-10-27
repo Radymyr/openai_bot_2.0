4c568c44b5505a370a8863911bcb19d50549a720
Radymyr <lobodaradymyr@gmail.com>
2025-10-27 22:01:48 +0200

    changed status to 200

--- api/webhook.js
+++ api/webhook.js
@@ -198,6 +198,6 @@ export default async (req, res) => {
     res.status(200).json({ ok: true });
   } catch (error) {
     console.error("Webhook error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(200).json({ ok: false });
   }
 };
